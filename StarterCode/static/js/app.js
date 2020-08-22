1d8781a2f2a20b307cb6bfb8e8e654037297a463
Sogra Memon <sogra.memon@dal.ca>
2020-08-22 14:58:10 -0400

    First part complete

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,12 @@
 // from data.js
 var tableData = data;
+tbody= d3.select("tbody");
 
 // YOUR CODE HERE!
+data.forEach((record) => {
+    var row = tbody.append("tr");
+    Object.entries(record).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
